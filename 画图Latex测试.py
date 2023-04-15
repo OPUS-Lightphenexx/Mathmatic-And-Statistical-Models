543335ce3b5515c6eae2fe4a5d46af0b14136d8d
OPUS-Lightphenexx <lightphenexx@outlook.com>
2023-04-15 18:22:18 +0800

    Update 画图Latex测试.py

--- 画图Latex测试.py
+++ 画图Latex测试.py
@@ -14,14 +14,4 @@ plt.text(-0.25,1.6,text_test,fontsize=40)
 
 plt.show()
 
-def Write_Latex_Matplotlib(x:str,y,z:str):
-    plt.text(0,0.9,x,fontsize = y)
-    plt.grid()
-    plt.title('Matplotlib Latex Text On Graph')
-    plt.xlabel(z)
-    plt.show()
-
-
-print(Write_Latex_Matplotlib(r'$\frac{1}{2}$ The Test Function',20))
-
 
