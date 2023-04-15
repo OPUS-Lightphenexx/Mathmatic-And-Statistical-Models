247cec9c3530e6fff200aa4ddfc1fed8ff72f540
OPUS-Lightphenexx <lightphenexx@outlook.com>
2023-04-15 16:55:18 +0800

    Create 画图Latex测试.py

---
+++ 画图Latex测试.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-1,1,100)
+y = x**2+x**3
+
+plt.plot(x,y)
+plt.grid()
+plt.title('Matplotlib Latex Text On Graph')
+plt.xlabel(r'$\frac{x^2}{2}$')
+
+text_test = r'$\frac{\sin{\frac{x^2}{2}}}{\cos{x}}$'
+plt.text(-0.25,1.6,text_test,fontsize=40)
+
+plt.show()
