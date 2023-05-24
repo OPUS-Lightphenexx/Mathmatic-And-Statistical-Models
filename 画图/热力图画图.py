b8ceb00c8a49ffae1b58b691ac97134f817ad8ae
OPUS-Lightphenexx <lightphenexx@outlook.com>
2023-05-24 12:59:28 +0800

    Update 热力图画图.py

--- 画图/热力图画图.py
+++ 画图/热力图画图.py
@@ -36,3 +36,16 @@ for i in range(7):
         text = plt.text(j, i, harvest[i, j],ha='center',va="center", color="black")
 plt.show()
 
+#single plot
+matrix = np.array([[0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
+                    [2.4, 0.0, 4.0, 1.0, 2.7, 0.0, 0.0],
+                    [1.1, 2.4, 0.8, 4.3, 1.9, 4.4, 0.0],
+                    [0.6, 0.0, 0.3, 0.0, 3.1, 0.0, 0.0],
+                    [0.7, 1.7, 0.6, 2.6, 2.2, 6.2, 0.0],
+                    [1.3, 1.2, 0.0, 0.0, 1, 3.2, 5.1],
+                    [0.1, 2.0, 0.0, 1.4, 0.0, 1.9, 6.3]])
+matrix_random = np.random.randint(0,4,(4,4))
+
+plt.imshow(matrix_random)
+plt.colorbar()
+plt.show()
